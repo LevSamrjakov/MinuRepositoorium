2f1bcc623de60b01f98c1cb39440cb0d7303f10d
Opilane TTHK <opilane@tthk.ee>
2025-10-23 10:18:41 +0300

    MinuProjekt Esimine Commit
    Esimene VS Commit.

---
+++ MinuProjekt/MinuProjekt/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MinuProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("See on minu uus projekt!");
+            Random Random = new Random();
+            int RandomNumber = Random.Next(1, 6);
+            int Number = 15;
+            Console.WriteLine(RandomNumber + Number);
+
+        }
+    }
+}
