c716f1902b44a09f06781160d33a83e07fdfac40
Opilane TTHK <opilane@tthk.ee>
2025-10-23 11:40:07 +0300

    SwitchWeekday Added.
    Later to be put in a different repository.

---
+++ SwitchWeekday/SwitchWeekday/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SwitchWeekday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Today is Monday. Monday is a work day.");
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Today is Tuesday. Tuesday is a work day.");
+                    break;
+
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("Today is Wednesday. Wednesday is a work day.");
+                    break;
+
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Today is Thursday. Thursday is a work day.");
+                    break;
+
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Today is Friday. Friday is a work day.");
+                    break;
+
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Today is Saturday. Saturday is a weekend.");
+                    break;
+
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Today is Sunday. Sunday is a weekend.");
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+            }
+        }
+    }
+}
